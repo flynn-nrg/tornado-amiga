7537836be0fa3f5aab6169a2baa6d12dfb3c8a38
Miguel Mendez <miguel@emerald-isle.eu>
2020-01-08 16:58:58 +0000

    Increase ddpcm decoder buffers. Tidy up the AHI wrapper and run the mixer at a lower rate. Still not 100% there.

--- src/amiga/audio_ahi.c
+++ src/amiga/audio_ahi.c
@@ -1,23 +1,22 @@
 /*
-Copyright (c) 2019 Miguel Mendez
-
-This software is provided 'as-is', without any express or implied warranty. In
-no event will the authors be held liable for any damages arising from the use of
-this software.
-
-Permission is granted to anyone to use this software for any purpose, including
-commercial applications, and to alter it and redistribute it freely, subject to
-the following restrictions:
-
-    1. The origin of this software must not be misrepresented; you must not
-claim that you wrote the original software. If you use this software in a
-product, an acknowledgment in the product documentation would be appreciated but
-is not required.
-
-    2. Altered source versions must be plainly marked as such, and must not be
-misrepresented as being the original software.
-
-    3. This notice may not be removed or altered from any source distribution.
+Copyright (c) 2019, Miguel Mendez. All rights reserved.
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <stdint.h>
@@ -38,8 +37,8 @@ misrepresented as being the original software.
 #include "tndo_assert.h"
 #include "tornado_settings.h"
 
-// Call the player function 50 times per second.
-#define PLAYER_FREQ 50
+// Call the player function 5 times per second.
+#define PLAYER_FREQ 5
 
 #define DEFAULT_SAMPLE_FREQ 22050
 
@@ -91,6 +90,8 @@ uint32_t audioAhiInit(demoParams *dp) {
     sampleFreq = 22050;
     break;
   }
+
+  numSamples = sampleFreq / PLAYER_FREQ;
 
   AHI_mp = CreateMsgPort();
   if (!AHI_mp) {
@@ -161,15 +162,17 @@ uint32_t audioAhiInit(demoParams *dp) {
             drivername, maxSamples, frameSize);
   }
 
-  numSamples = maxSamples;
+  if (maxSamples < numSamples) {
+    numSamples = maxSamples;
+  }
 
   int bufferSize = numSamples;
 
   lBuffer.ahisi_Length = bufferSize;
   rBuffer.ahisi_Length = bufferSize;
 
-  lBuffer.ahisi_Address = tndo_malloc(bufferSize * 2, 0);
-  rBuffer.ahisi_Address = tndo_malloc(bufferSize * 2, 0);
+  lBuffer.ahisi_Address = tndo_malloc(numSamples * frameSize, 0);
+  rBuffer.ahisi_Address = tndo_malloc(numSamples * frameSize, 0);
 
   tndo_assert(lBuffer.ahisi_Address);
   tndo_assert(rBuffer.ahisi_Address);
