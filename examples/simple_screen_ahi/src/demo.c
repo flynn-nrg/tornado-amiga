8742c0eec916dff44c7cd14263c6941d81412b82
Miguel Mendez <miguel@emerald-isle.eu>
2023-10-15 16:31:15 +0100

    fix scale fetching in decoder. update examples to use realtime unpacking again.

--- examples/simple_screen_ahi/src/demo.c
+++ examples/simple_screen_ahi/src/demo.c
@@ -170,7 +170,7 @@ void loaderCallback(int loaded, int total) {
 
 void demoSettings(demoParams *dp) {
   dp->minCPU = MIN_CPU_040;
-  dp->tornadoOptions = LOGGING | INSTALL_LEVEL3 | USE_AUDIO | DDPCM_UNPACK;
+  dp->tornadoOptions = LOGGING | INSTALL_LEVEL3 | USE_AUDIO;
 #ifdef __DEBUG_CODE
   dp->tornadoOptions |= VERBOSE_DEBUGGING | MEMORY_PROFILING;
 #endif
